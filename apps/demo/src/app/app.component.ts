aa81f142533108d9a7521493de3a5df6a155ff18
Matěj Chalk <matej.chalk@flowup.cz>
2021-04-12 10:39:05 +0200

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contentful-rich-text-angular-renderer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
