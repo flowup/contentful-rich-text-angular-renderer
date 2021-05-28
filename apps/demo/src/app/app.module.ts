789f54896de4f919a9b888f672ab809e83d15354
Matěj Chalk <matej.chalk@flowup.cz>
2021-05-28 17:44:46 +0200

    feat(lib): make library publishable and buildable

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { CfRichTextModule } from '@contentful-rich-text-angular-renderer/contentful-rich-text-angular-renderer';
+import { CfRichTextModule } from '@flowup/contentful-rich-text-angular-renderer';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
 import { AppComponent } from './app.component';
 import { CustomRichTextComponent } from './components/custom-rich-text/custom-rich-text.component';
