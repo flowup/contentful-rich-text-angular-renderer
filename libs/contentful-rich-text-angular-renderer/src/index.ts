bf6208e4699235a625da300c524bf75c2a769717
Vojtech Masek <vojtech@flowup.cz>
2025-07-10 17:26:48 +0200

    fix: explicitly export all types in the index file

--- libs/contentful-rich-text-angular-renderer/src/index.ts
+++ libs/contentful-rich-text-angular-renderer/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/rich-text-children.directive';
 export * from './lib/rich-text-document.component';
 export * from './lib/rich-text-mark.directive';
 export * from './lib/rich-text-node.directive';
+export * from './lib/types';
